853339db986505ebebf1456b01a99eb54540d3c6
knights <farhan@alphasaur.com>
2017-11-26 01:09:49 +0400

    View Files Added

--- config/config.js
+++ config/config.js
@@ -1,2 +1,2 @@
-var NODE_ENV = process.env.NODE_ENV ? process.env.NODE_ENV : 'development';
-module.exports = require('./'+NODE_ENV +'.json');
+var NODE_ENV = process.env.NODE_ENV ? process.env.NODE_ENV : 'development'
+module.exports = require('./' + NODE_ENV + '.json')
